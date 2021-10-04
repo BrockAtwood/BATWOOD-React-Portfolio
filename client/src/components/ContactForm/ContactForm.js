0e90f45d0755586347371efba20a040a9f93e7a1
Brock Atwood <brock.atwood@du.edu>
2021-10-03 21:45:56 -0600

    adding email validator with helper and working on contactForm

--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
+import { ValidateEmail } from "";
 
 //capture First name and Last name from user input
 
--- client/src/components/Gallery/Gallery.js
+++ client/src/components/Gallery/Gallery.js
@@ -1,52 +1,9 @@
 import React from "react";
 import "./gallery.css";
+//importing the Navbar from react-bootstrap
+import { Card, Button } from "react-bootstrap";
 
-//component is responsible for displaying cards of images, products in our database that have images we want to display on page load
-
-const projects = [
-  {
-    id: 1,
-    image: "/images/Delishery.png",
-    title: "Delishery",
-    link: "https://adrianauch.github.io/Delishery/",
-    github: "https://github.com/adrianauch/Delishery",
-  },
-  {
-    id: 2,
-    image: "/images/sideswipe.png",
-    title: "SideSwipe",
-    link: "https://thawing-mesa-73583.herokuapp.com/",
-    github: "https://github.com/AdamKuemmel/CARS_project2",
-  },
-  {
-    id: 3,
-    image: "/images/fitness.png",
-    title: "Fitness Tracker",
-    link: "https://serene-sands-75725.herokuapp.com/",
-    github: "https://github.com/BrockAtwood/BATWOOD-Fitness-Tracker",
-  },
-  {
-    id: 4,
-    image: "/images/Notetaker.png",
-    title: "Note-Taker",
-    link: "https://afternoon-harbor-31606.herokuapp.com/",
-    github: "https://github.com/BrockAtwood/NOTE-TAKER-FINAL-SUBMIT",
-  },
-  {
-    id: 5,
-    image: "/images/workday.png",
-    title: "Work Day Scheduler",
-    link: "https://brockatwood.github.io/Work-Day-Planner/",
-    github: "https://github.com/BrockAtwood/Work-Day-Planner",
-  },
-  {
-    id: 6,
-    image: "/images/password.png",
-    title: "Password Generator",
-    link: "https://brockatwood.github.io/Batwood-Password-Generator/",
-    github: "https://github.com/BrockAtwood/Batwood-Password-Generator",
-  },
-];
+//component is responsible for displaying cards of images, products in our database that have images we want to display on page loa
 
 const Gallery = () => {
   //grab cards from the database, let me call map on that array of objects, and inside of that callback function i can call Card Component
@@ -54,7 +11,157 @@ const Gallery = () => {
   return (
     <>
       <h1 className="title">My Top 6 Portfolio Pieces</h1>
-      <section></section>
+      <section className="container-fluid d-flex justify-content-center">
+        <Card class="cbbody" style={{ width: "18rem" }}>
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={process.env.PUBLIC_URL + "/Images/Delishery.png"}
+          />
+          <Card.Body>
+            <Card.Title class="projecttitle">Delishery</Card.Title>
+            <Button
+              href="https://adrianauch.github.io/Delishery/"
+              target="_blank"
+              variant="outline-primary"
+            >
+              Deployed App
+            </Button>
+            <Button
+              href="https://github.com/adrianauch/Delishery"
+              target="_blank"
+              variant="outline-primary"
+            >
+              GitHub Link
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card class="cbbody" style={{ width: "18rem" }}>
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={process.env.PUBLIC_URL + "/Images/sideswipe.png"}
+          />
+          <Card.Body>
+            <Card.Title class="projecttitle">SideSwipe</Card.Title>
+            <Button
+              href="https://adrianauch.github.io/Delishery/"
+              target="_blank"
+              variant="outline-primary"
+            >
+              Deployed App
+            </Button>
+            <Button
+              href="https://github.com/adrianauch/Delishery"
+              target="_blank"
+              variant="outline-primary"
+            >
+              GitHub Link
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card class="cbbody" style={{ width: "18rem" }}>
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={process.env.PUBLIC_URL + "/images/fitness.png"}
+          />
+          <Card.Body>
+            <Card.Title class="projecttitle">Fitness Tracker</Card.Title>
+            <Button
+              href="https://serene-sands-75725.herokuapp.com/"
+              target="_blank"
+              variant="outline-primary"
+            >
+              Deployed App
+            </Button>
+            <Button
+              href="https://github.com/BrockAtwood/BATWOOD-Fitness-Tracker"
+              target="_blank"
+              variant="outline-primary"
+            >
+              GitHub Link
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card class="cbbody" style={{ width: "18rem" }}>
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={process.env.PUBLIC_URL + "/images/Notetaker.png"}
+          />
+          <Card.Body>
+            <Card.Title class="projecttitle">Fitness Tracker</Card.Title>
+            <Button
+              href="https://afternoon-harbor-31606.herokuapp.com/"
+              target="_blank"
+              variant="outline-primary"
+            >
+              Deployed App
+            </Button>
+            <Button
+              href="https://github.com/BrockAtwood/NOTE-TAKER-FINAL-SUBMIT"
+              target="_blank"
+              variant="outline-primary"
+            >
+              GitHub Link
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card class="cbbody" style={{ width: "18rem" }}>
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={process.env.PUBLIC_URL + "/images/workday.png"}
+          />
+          <Card.Body>
+            <Card.Title class="projecttitle">Work Day Scheduler</Card.Title>
+            <Button
+              href="https://brockatwood.github.io/Work-Day-Planner/"
+              target="_blank"
+              variant="outline-primary"
+            >
+              Deployed App
+            </Button>
+            <Button
+              href="https://github.com/BrockAtwood/Work-Day-Planner"
+              target="_blank"
+              variant="outline-primary"
+            >
+              GitHub Link
+            </Button>
+          </Card.Body>
+        </Card>
+
+        <Card class="cbbody" style={{ width: "18rem" }}>
+          <Card.Img
+            class="samesize"
+            variant="top"
+            src={process.env.PUBLIC_URL + "/images/password.png"}
+          />
+          <Card.Body>
+            <Card.Title class="projecttitle">Password Generator</Card.Title>
+            <Button
+              href="https://brockatwood.github.io/Batwood-Password-Generator/"
+              target="_blank"
+              variant="outline-primary"
+            >
+              Deployed App
+            </Button>
+            <Button
+              href="https://github.com/BrockAtwood/Batwood-Password-Generator"
+              target="_blank"
+              variant="outline-primary"
+            >
+              GitHub Link
+            </Button>
+          </Card.Body>
+        </Card>
+      </section>
     </>
   );
 };
